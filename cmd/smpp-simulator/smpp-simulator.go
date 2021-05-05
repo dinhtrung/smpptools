4230ed23954f63538ac9316dd3146b9f08270f2f
Trung Nguyen Dinh <nguyendinhtrung141@gmail.com>
2021-05-05 23:31:00 +0700

    Add placeholders first

--- cmd/smpp-simulator/smpp-simulator.go
+++ cmd/smpp-simulator/smpp-simulator.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"log"
 	"flag"
+	"log"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/markbates/pkger"
-	
+
 	"github.com/dinhtrung/smpptools/internal/app"
+	"github.com/dinhtrung/smpptools/internal/app/smpp-simulator/web/api"
 )
+
 var configFile string
 
 func main() {
@@ -20,8 +26,7 @@ func main() {
 	app.ConfigWatch(configFile)
 
 	srv := fiber.New(fiber.Config{
-		BodyLimit:    50 * 1024 * 1024,
-		ErrorHandler: helpers.ProblemJSONErrorHandle,
+		BodyLimit: 50 * 1024 * 1024,
 	})
 	staticAsset := filesystem.New(filesystem.Config{
 		Next: func(c *fiber.Ctx) bool {
@@ -30,8 +35,29 @@ func main() {
 		Root: pkger.Dir("/web"),
 	})
 	srv.Use(staticAsset)
+	srv.Use(logger.New(logger.Config{
+		// Format:     "{\"timestamp\":\"${time}\", \"status\":${status}, \"account\":\"${locals:account}\", \"method\":\"${method}\", \"path\":\"${path}\", \"body\":${body}}\n",
+		// Format:     "${time} ${status} ${locals:account} ${method} ${path} '${queryParams}' '${body}'\n",
+		TimeFormat: "2006-01-02T15:04:05-0700",
+	}))
 
 	setupRoutes(srv)
 
 	log.Fatal(srv.Listen(app.Config.String("http.listen")))
+}
+
+// setupRoutes setup the route for application
+func setupRoutes(app *fiber.App) {
+	app.Get("api/settings", api.GetSettings)
+	app.Post("api/settings", api.SaveSettings)
+	app.Get("api/start-session", api.StartSession)
+	app.Post("api/start-session", api.StartSession)
+	app.Get("api/stop-session", api.StopSession)
+	app.Get("api/refresh-state", api.RefreshState)
+	app.Get("api/send-bad-packet", api.SendBadPacket)
+	app.Get("api/submit-message", api.SubmitMessage)
+	app.Post("api/submit-message", api.SubmitMessage)
+	app.Get("api/bulk-sending-random", api.BulkSendingRandom)
+	app.Get("api/stop-bulk-sending", api.StopBulkSending)
+	app.Post("api/convert-text", api.ConvertText)
 }
---
+++ internal/app/smpp-simulator/web/api/settings.go
@@ -0,0 +1,34 @@
+package api
+
+import "github.com/gofiber/fiber/v2"
+
+func GetSettings(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func SaveSettings(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func StartSession(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func StopSession(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func RefreshState(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func SendBadPacket(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func SubmitMessage(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func BulkSendingRandom(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func StopBulkSending(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
+func ConvertText(c *fiber.Ctx) error {
+	return fiber.ErrNotImplemented
+}
